6de46d4cee1d30c627e79613e45e332d71332cb0
willotavio <willaotavio@gmail.com>
2023-11-24 10:01:51 -0300

    Fix types.ts

---
+++ types.ts
@@ -0,0 +1,16 @@
+export type User = {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+  aud?: string | null | undefined;
+  azp?: string | null | undefined;
+  email_verified?: boolean | null | undefined;
+  exp?: number | null | undefined;
+}
+
+export type Post = {
+  id: string;
+  content: string;
+  user: string;
+  date: string;
+}
