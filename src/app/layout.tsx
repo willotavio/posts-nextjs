bb01553d862a848ab6e277623d4cbf702c9f79ae
willotavio <willaotavio@gmail.com>
2023-11-28 15:15:27 -0300

    Add AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './ui/Navbar'
 import { ReduxProvider } from './redux/provider'
+import { AuthProvider } from './context/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - Post ur shits",
-    default: "Post ur shits",
+    template: "%s - Post ur stuff",
+    default: "Post ur stuff",
   },
   description: "For no reason",
 }
@@ -24,9 +25,11 @@ export default function RootLayout({
       <body className={`bg-gray-50 ${inter.className}`}>
         <Navbar />
         <div className="mt-20">
-          <ReduxProvider>
-            {children}
-          </ReduxProvider>
+          <AuthProvider>
+            <ReduxProvider>
+              {children}
+            </ReduxProvider>
+          </AuthProvider>
         </div>
       </body>
     </html>
